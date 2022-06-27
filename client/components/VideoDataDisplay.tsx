27c387a768462392d9141e7e10a9414b5b7b6406
Batin EVIRGEN <batinevirgen@gmail.com>
2022-06-27 13:33:03 +0300

    client: add focus indicator to download links

--- client/components/VideoDataDisplay.tsx
+++ client/components/VideoDataDisplay.tsx
@@ -19,7 +19,7 @@ const FormatDisplay = ({ formats }: FormatDisplayProps) => {
   const formatsDOM = formats.map((format) => (
     <a
       href={format.url}
-      className="bg-neutral-900 group p-2 rounded-md transition duration-100 hover:text-neutral-200 border-2 border-neutral-700 hover:border-blue-600"
+      className="bg-neutral-900 group p-2 rounded-md transition focus:outline-none focus:ring-2 focus:ring-blue-600 duration-100 hover:text-neutral-200 border-2 border-neutral-700 hover:border-blue-600"
     >
       <p className="opacity-100 group-hover:opacity-0 transition duration-100">
         {resolutionToReadable(format.resolution)}
