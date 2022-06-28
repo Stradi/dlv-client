88f3a07cb55e2337351b2feeebb9273853338708
Batin EVIRGEN <batinevirgen@gmail.com>
2022-06-28 20:48:57 +0300

    client: add next-seo

--- client/pages/[provider].tsx
+++ client/pages/[provider].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import { NextSeo } from "next-seo";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -23,63 +24,70 @@ const ProviderPage = ({ provider }: ProviderPageProps) => {
   const [videoData, setVideoData] = useState(null);
 
   return (
-    <div>
-      <Downloader
-        title={`${provider} Downloader`}
-        onVideoDataAcquired={(data) => setVideoData(data)}
-        onDownloadButtonClicked={() => setVideoData(null)}
+    <>
+      <NextSeo
+        title={`Download ${provider} Video`}
+        description={`Free downloader for ${provider} and more. Extract audio from a ${provider} video or download full video for free.`}
       />
-      {videoData && (
-        <>
-          <br></br>
-          <VideoDataDisplay data={videoData} />
-        </>
-      )}
-      <br></br>
-      <Markdown>
-        <h2>Download {provider} Video</h2>
-        <p>
-          Have you ever wanted to download a funny
-          {providerToSpecialTextComponent(provider)}
-          video to send your friend or family.
-        </p>
-        <p>
-          Have you ever wanted to download a sound from{" "}
-          {providerToSpecialTextComponent(provider)}.
-        </p>
-        <p>You've come to the right place.</p>
-        <p>
-          Download YouTube Video is a free service that allows you to download
-          <YouTube />,<Dailymotion />
-          or
-          <Vimeo />
-          videos.
-        </p>
-        <p>
-          We make downloading videos from
-          {providerToSpecialTextComponent(provider)}
-          easy... It's all two clicks away.
-        </p>
-        <p>The best part is our service is 100% free.</p>
-        <h2>How to use?</h2>
-        <p>Using our downloader is fully free and easy.</p>
-        <p>
-          Just paste the URL of the video in the search box up there and click
-          Download now.
-        </p>
-        <Image src={imgTutorial1} className="rounded-md" />
-        <p>After some moment, you will be greeted with a panel like below.</p>
-        <Image src={imgTutorial2} className="rounded-md" />
-        <p>
-          In this panel, you are able to download the Video+Audio or Only Audio.
-        </p>
-        <p>
-          Click the desired option and let us do the magic and start the
-          download.
-        </p>
-        <Image src={imgTutorial3} className="rounded-md" />
-      </Markdown>
-    </div>
+      <div>
+        <Downloader
+          title={`${provider} Downloader`}
+          onVideoDataAcquired={(data) => setVideoData(data)}
+          onDownloadButtonClicked={() => setVideoData(null)}
+        />
+        {videoData && (
+          <>
+            <br></br>
+            <VideoDataDisplay data={videoData} />
+          </>
+        )}
+        <br></br>
+        <Markdown>
+          <h2>Download {provider} Video</h2>
+          <p>
+            Have you ever wanted to download a funny
+            {providerToSpecialTextComponent(provider)}
+            video to send your friend or family.
+          </p>
+          <p>
+            Have you ever wanted to download a sound from{" "}
+            {providerToSpecialTextComponent(provider)}.
+          </p>
+          <p>You've come to the right place.</p>
+          <p>
+            Download YouTube Video is a free service that allows you to download
+            <YouTube />,<Dailymotion />
+            or
+            <Vimeo />
+            videos.
+          </p>
+          <p>
+            We make downloading videos from
+            {providerToSpecialTextComponent(provider)}
+            easy... It's all two clicks away.
+          </p>
+          <p>The best part is our service is 100% free.</p>
+          <h2>How to use?</h2>
+          <p>Using our downloader is fully free and easy.</p>
+          <p>
+            Just paste the URL of the video in the search box up there and click
+            Download now.
+          </p>
+          <Image src={imgTutorial1} className="rounded-md" />
+          <p>After some moment, you will be greeted with a panel like below.</p>
+          <Image src={imgTutorial2} className="rounded-md" />
+          <p>
+            In this panel, you are able to download the Video+Audio or Only
+            Audio.
+          </p>
+          <p>
+            Click the desired option and let us do the magic and start the
+            download.
+          </p>
+          <Image src={imgTutorial3} className="rounded-md" />
+        </Markdown>
+      </div>
+    </>
   );
 };
 
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { DefaultSeo } from "next-seo";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <DefaultSeo
+        defaultTitle="Download YouTube Video"
+        description="Free downloader for YouTube, Dailymotion and Vimeo. Extract audio from a video or download full video for free, forever."
+      />
       <Component {...pageProps} />
     </Layout>
   );
