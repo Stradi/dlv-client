82b22a92543c41be3e3033b4a671057addcb657c
Batin EVIRGEN <batinevirgen@gmail.com>
2022-07-01 21:59:43 +0300

    add vimeo image cdn

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.ytimg.com', 's1.dmcdn.net', 's2.dmcdn.net'],
+    domains: ['i.ytimg.com', 's1.dmcdn.net', 's2.dmcdn.net', 'i.vimeocdn.com'],
   },
 };
 
