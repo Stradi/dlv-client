169be0152ed3ae3bfc916cd71fe1e50769a69cc1
Batin EVIRGEN <batinevirgen@gmail.com>
2022-07-03 14:54:03 +0300

    dont send analytics event on dev env

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,15 +1,20 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 export const pageView = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if (process.env.NODE_ENV === "production") {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+    console.log("Sent pageview");
+  }
 };
 
 export const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
+  if (process.env.NODE_ENV === "production") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+  }
 };
